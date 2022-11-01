de5efacd44c7bd77756f4e0f1f4fc0a6596c2173
Adam Cox <adamcox.dev@gmail.com>
2022-11-01 15:27:13 -0600

    First commit

---
+++ model.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    username = db.Column(db.String(255), unique = True, nullable = False)
+    password = db.Column(db.String(255), nullable = False)
+
+class Project(db.Model):
+
+    __tablename__ = "projects"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    project_name = db.Column(db.String(255), nullable = False)
+    description = db.Column(db.String(255), nullable = True)
+    completed = db.Column(db.Boolean, default = False)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable = False)
