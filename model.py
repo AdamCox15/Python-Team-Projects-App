29e6e53e431c1461e8d5a3afda7f96915a33ffd0
Adam Cox <adamcox.dev@gmail.com>
2022-11-02 17:01:07 -0600

    Part 3 questions 2-4

--- model.py
+++ model.py
@@ -17,6 +17,8 @@ class User(db.Model):
     username = db.Column(db.String(255), unique = True, nullable = False)
     password = db.Column(db.String(255), nullable = False)
 
+    teams = db.relationship("Team", backref = "user", lazy = True)
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -29,6 +31,11 @@ class Team(db.Model):
     team_name = db.Column(db.String(255), unique = True, nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
 
+    def __init__(self, team_name, user_id):
+        self.team_name = team_name
+        self.user_id = user_id
+
+
 class Project(db.Model):
 
     __tablename__ = "projects"
@@ -38,6 +45,14 @@ class Project(db.Model):
     description = db.Column(db.String(255), nullable = True)
     completed = db.Column(db.Boolean, default = False)
     team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable = False)
+
+    def __init__(self, project_name, completed, team_id, **kwargs):
+        self.project_name = project_name
+        self.completed = completed
+        self.team_id = team_id
+        
+        if "description" in kwargs:
+            self.description = kwargs["description"]
 
 if __name__ == "__main__":
     from flask import Flask
